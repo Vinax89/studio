e249ea6e67f72b6504797d4b6baa45f3cc148835
Vinax89 <migz89star@gmail.com>
2025-08-28 05:43:27 -0700

    refactor: migrate map worker to TypeScript

--- src/lib/mapWorker.js
+++
@@ -1,9 +0,0 @@
-const { parentPort } = require("node:worker_threads")
-
-function square(numbers) {
-  return numbers.map(n => n * n)
-}
-
-parentPort.on("message", numbers => {
-  parentPort.postMessage(square(numbers))
-})
