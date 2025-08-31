e40538e3e1f4e1f9eae0dc2decf47ac8574ba208
Vinax89 <migz89star@gmail.com>
2025-08-31 11:35:31 -0700

    feat: add basic logger

---
+++ src/lib/logger.ts
@@ -0,0 +1,7 @@
+export const logger = {
+  error: (...args: any[]) => console.error(...args),
+  warn: (...args: any[]) => console.warn(...args),
+  info: (...args: any[]) => console.info(...args),
+};
+
+export default logger;
